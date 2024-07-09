e28211a2cedd532f4fa34475bf51a4b51757b71c
Artem Nechaev <ar.ne4aew@yandex.ru>
2024-07-09 14:14:28 +0300

    add rtk query and redux toolkit

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,7 +1,8 @@
-import { getCategories } from "../../api/apiNews";
 import { useTheme } from "../../context/ThemeContext";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { CategoriesApiResponse, IFilters } from "../../interfaces";
+import { IFilters } from "../../interfaces";
+import { useAppDispatch } from "../../store";
+import { useGetCategoriesQuery } from "../../store/services/newsApi";
+import { setFilters } from "../../store/slices/newsSlice";
 import Categories from "../Categories/Categories";
 import Search from "../Search/Search";
 import Slider from "../Slider/Slider";
@@ -9,23 +10,23 @@ import styles from "./styles.module.css";
 
 interface Props {
   filters: IFilters;
-  changeFilters: (key: string, value: string | number | null) => void;
 }
 
-const NewsFilters = ({ filters, changeFilters }: Props) => {
+const NewsFilters = ({ filters }: Props) => {
   const { isDark } = useTheme();
-  const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(
-    getCategories
-  );
+
+  const { data } = useGetCategoriesQuery(null);
+
+  const dispatch = useAppDispatch();
 
   return (
     <header className={styles.filters}>
-      {dataCategories ? (
+      {data ? (
         <Slider isDark={isDark}>
           <Categories
-            categories={dataCategories.categories}
+            categories={data.categories}
             setSelectedCategory={(category) =>
-              changeFilters("category", category)
+              dispatch(setFilters({ key: "category", value: category }))
             }
             selectedCategory={filters.category}
           />
@@ -33,7 +34,9 @@ const NewsFilters = ({ filters, changeFilters }: Props) => {
       ) : null}
       <Search
         keywords={filters.keywords}
-        setKeywords={(keywords) => changeFilters("keywords", keywords)}
+        setKeywords={(keywords) =>
+          dispatch(setFilters({ key: "keywords", value: keywords }))
+        }
       />
     </header>
   );
